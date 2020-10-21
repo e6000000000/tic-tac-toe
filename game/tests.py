c619720abdb84a8901945427772181928b34f689
e6000000000 <nick100i1@mail.ru>
2020-10-21 15:24:20 +0300

    now tests working

--- game/tests.py
+++ game/tests.py
@@ -7,86 +7,78 @@ from .enums import GameStatus
 
 class TestUM(unittest.TestCase):
     def setUp(self):
-        pass
+        self.field = TicTacToeField()
 
     def tearDown(self):
         pass
 
     def test_empty_field_generation(self):
-        field = TicTacToeField()
-
-        self.assertEqual(field.get(), [
+        self.assertEqual(self.field.get(), [
             [' ', ' ', ' '],
             [' ', ' ', ' '],
             [' ', ' ', ' '],
         ])
 
     def test_first_move(self):
-        field = TicTacToeField()
-        field.move_X(1, 1)
+        self.field.move_X(1, 1)
 
-        self.assertEqual(field.get(), [
+        self.assertEqual(self.field.get(), [
             [' ', ' ', ' '],
             [' ', 'X', ' '],
             [' ', ' ', ' '],
         ])
 
     def test_second_move(self):
-        field = TicTacToeField()
-        field.move_X(1, 1)
-        field.move_O(0, 0)
+        self.field.move_X(1, 1)
+        self.field.move_O(0, 0)
 
-        self.assertEqual(field.get(), [
+        self.assertEqual(self.field.get(), [
             ['O', ' ', ' '],
             [' ', 'X', ' '],
             [' ', ' ', ' '],
         ])
 
     def test_X_win(self):
-        field = TicTacToeField()
-        field.move_X(1, 1)
-        field.move_O(0, 0)
-        field.move_X(0, 1)
-        field.move_O(1, 0)
-        field.move_X(2, 1)
+        self.field.move_X(1, 1)
+        self.field.move_O(0, 0)
+        self.field.move_X(0, 1)
+        self.field.move_O(1, 0)
+        self.field.move_X(2, 1)
 
-        self.assertEqual(field.status, GameStatus.X_WIN)
+        self.assertEqual(self.field.status, GameStatus.X_WIN)
 
     def test_O_win(self):
-        field = TicTacToeField()
-        field.move_X(2, 2)
-        field.move_O(1, 1)
-        field.move_X(0, 0)
-        field.move_O(0, 1)
-        field.move_X(1, 0)
-        field.move_O(2, 1)
+        self.field.move_X(2, 2)
+        self.field.move_O(1, 1)
+        self.field.move_X(0, 0)
+        self.field.move_O(0, 1)
+        self.field.move_X(1, 0)
+        self.field.move_O(2, 1)
 
-        self.assertEqual(field.status, GameStatus.O_WIN)
+        self.assertEqual(self.field.status, GameStatus.O_WIN)
 
     def test_in_progress(self):
-        field = TicTacToeField()
-        field.move_X(2, 2)
-        field.move_O(1, 1)
-        field.move_X(0, 0)
-        field.move_O(0, 1)
-        field.move_X(1, 0)
+        self.field.move_X(2, 2)
+        self.field.move_O(1, 1)
+        self.field.move_X(0, 0)
+        self.field.move_O(0, 1)
+        self.field.move_X(1, 0)
 
-        self.assertEqual(field.status, GameStatus.IN_PROGRESS)
+        self.assertEqual(self.field.status, GameStatus.IN_PROGRESS)
 
     def test_draw(self):
-        field = TicTacToeField()
-        field.move_X(0, 0)
-        field.move_O(0, 1)
-        field.move_X(0, 2)
-        field.move_O(2, 0)
-        field.move_X(2, 1)
-        field.move_O(2, 2)
-        field.move_X(1, 0)
-        field.move_O(1, 1)
-        field.move_X(1, 2)
+        self.field.move_X(0, 0)
+        self.field.move_O(0, 1)
+        self.field.move_X(0, 2)
+        self.field.move_O(2, 0)
+        self.field.move_X(2, 1)
+        self.field.move_O(2, 2)
+        self.field.move_X(1, 0)
+        self.field.move_O(1, 1)
+        self.field.move_X(1, 2)
 
-        self.assertEqual(field.status, GameStatus.DRAW)
+        self.assertEqual(self.field.status, GameStatus.DRAW)
 
 
-if __name__ == '__main__':
+def main():
     unittest.main()
