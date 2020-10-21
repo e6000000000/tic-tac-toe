30237e193026f01951a2ac087738cbfad69c61ee
e6000000000 <nick100i1@mail.ru>
2020-10-21 03:33:33 +0300

    replace AsyncConsumer to AsyncWebsocketConsumer

--- game/consumers.py
+++ game/consumers.py
@@ -1,4 +1,5 @@
-from channels.consumer import AsyncConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.exceptions import DenyConnection
 import asyncio
 import json
 
@@ -6,52 +7,40 @@ from .game_sessions import GameSessions
 from .exceptions import MoveUnableException
 
 
-class GameConsumer(AsyncConsumer):
-    async def websocket_connect(self, event):
+class GameConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.session_id = self.scope['url_route']['kwargs']['session_id']
         self.player_id = self.scope['url_route']['kwargs']['player_id']
         
-        """try:
+        try:
             GameSessions.get_by_id(self.session_id)
         except:
-            await self.send({
-                'type': 'websocket.close'
-            })
-            return
-        """
-
-        await self.send({
-            'type': 'websocket.accept'
-        })
+            raise DenyConnection('invalid session id')
         
-
         await self.channel_layer.group_add(
             self.session_id.__str__(),
             self.channel_name
         )
 
+        await self.accept()
         await self.send_game_data('')
 
     async def send_game_data(self, event):
         game_session = GameSessions.get_by_id(self.session_id)
 
-        gamedata = {
+        data = {
             'game_field': game_session.game_field,
             'x_win_count': game_session.x_win_count,
             'o_win_count': game_session.o_win_count,
             'draw_count': game_session.draw_count
         }
+        await self.send(text_data=json.dumps(data))
 
-        await self.send({
-            'type': 'websocket.send',
-            'text': json.dumps(gamedata)
-        })
-
-    async def websocket_receive(self, text_data):
+    async def receive(self, text_data):
         try:
-            received_data = json.loads(text_data['text'])
+            received_data = json.loads(text_data)
         except Exception as e:
-            print(e, text_data)
+            print(type(e), e, text_data)
             return
 
         game_session = GameSessions.get_by_id(self.session_id)
@@ -64,7 +53,7 @@ class GameConsumer(AsyncConsumer):
         except MoveUnableException:
             return
         except Exception as e:
-            print(e, received_data)
+            print(type(e), e, received_data)
             return
 
         await self.channel_layer.group_send(
@@ -75,4 +64,7 @@ class GameConsumer(AsyncConsumer):
         )
 
     async def websocket_disconnect(self, event):
-        pass
+        await self.channel_layer.group_discard(
+            self.session_id.__str__(),
+            self.channel_name
+        )
