d1838ff5cbed365357cf6dcd5cc55b814f415cb3
e6000000000 <nick100i1@mail.ru>
2020-10-21 23:05:25 +0300

    added lobby

--- game/urls.py
+++ game/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('<int:session_id>/<int:player_id>', views.game, name='game'),
-    path('', views.game_create, name='game_create'),
+    path('ai/<str:play_side>', views.game_with_ai, name='ai_game'),
+    path('create/<str:play_side>', views.game_create, name='game_create'),
+    path('', views.lobby, name='lobby'),
 ]
--- game/views.py
+++ game/views.py
@@ -12,14 +12,29 @@ def game(request, session_id, player_id):
         friend_player_id = game_session.X_id
     else:
         friend_player_id = 0
-    friend_link = reverse('game_create') + f'{session_id}/{friend_player_id}'
+    friend_link = reverse('lobby') + f'{session_id}/{friend_player_id}'
     return render(request, 'game.html', {
         'friend_link': friend_link
     })
 
-def game_create(request):
+def game_with_ai(request, play_side):
+    """play game with ai
+    """
+    return redirect('lobby')
+
+def game_create(request, play_side):
     """create game session
     """
     new_game_id = GameSessions.new()
-    player_id = GameSessions.get_by_id(new_game_id).X_id
+    if play_side.lower() == 'x':
+        player_id = GameSessions.get_by_id(new_game_id).X_id
+    elif play_side.lower() == 'o':
+        player_id = GameSessions.get_by_id(new_game_id).O_id
+    else:
+        raise ValueError(f'play_side should be "X" or "O", not {play_side}')
     return redirect('game', new_game_id, player_id)
+
+def lobby(request):
+    """lobby from where player start
+    """
+    return render(request, 'lobby.html')
