ecb9d49ce35e7c1769c9eb83bc7986d6629f1309
e6000000000 <nick100i1@mail.ru>
2020-10-25 23:38:53 +0300

    update lobby ui

--- static/js/lobby.js
+++ static/js/lobby.js
@@ -2,12 +2,32 @@ function ws_search_on_message(e) {
     window.location.replace(e.data);
 }
 
+var search_label = document.getElementById('search_label');
+var seconds = 0;
+var minutes = 0;
+function display_search_time() {
+    seconds += 1;
+    if (seconds >= 60) {
+        minutes += 1;
+        seconds = 0;
+    }
+    if (minutes <= 0)
+    {
+        search_label.innerText = ('search: ' + seconds);
+    }
+    else {
+        search_label.innerText = ('search: ' + minutes + ':' + seconds);
+    }
+}
+
 document.getElementById('search_x').onclick = function(e) {
     var search_ws = new WebSocket(search_ws_base_url + '/X');
     search_ws.onmessage = ws_search_on_message;
+    setInterval(display_search_time, 1000);
 }
 
 document.getElementById('search_o').onclick = function(e) {
     var search_ws = new WebSocket(search_ws_base_url + '/O');
     search_ws.onmessage = ws_search_on_message;
+    setInterval(display_search_time, 1000);
 }
